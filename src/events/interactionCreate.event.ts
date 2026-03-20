6859525b8848f91eef64e8093f6d06c5e329e883
@felipe.micheletti_hadrons <felipe.micheletti_hadrons@prestador.globo>
2026-03-20 12:17:02 -0300

    Feat: Upgrate event to listening embed response

--- src/events/interactionCreate.event.ts
+++ src/events/interactionCreate.event.ts
@@ -4,29 +4,33 @@ import { AppError } from "../shared/errors/app.error";
 
 export default {
     name: Events.InteractionCreate,
-    once: false, 
+    once: false,
 
     async execute(interaction: Interaction) {
-        if (!interaction.isChatInputCommand()) return;
-
-        const command = (interaction.client as Client & { commands: Map<string, any> }).commands.get(interaction.commandName);
-
-        if (!command) {
-            logger.warn(`Comando não encontrado: ${interaction.commandName}`);
-            return;
-        }
-
         try {
-            await command.execute(interaction);
+            if (interaction.isChatInputCommand()) {
+                const command = (interaction.client as Client & { commands: Map<string, any> }).commands.get(interaction.commandName);
+
+                if (!command) return logger.warn(`Comando não encontrado: ${interaction.commandName}`);
+
+                return await command.execute(interaction);
+            }
+
+            if (interaction.isStringSelectMenu()) {
+                //* Adicionar aqui eventos de respostas
+                return;
+            }
         } catch (error) {
-            logger.error(`Erro ao executar o comando ${interaction.commandName}`, error);
+            logger.error("Erro ao processar interação", error);
 
-            const message = error instanceof AppError ? error.message : 'Ocorreu um erro ao executar este comando.';
+            const message = error instanceof AppError ? error.message : "Ocorreu um erro ao processar esta interação.";
 
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: message, ephemeral: true });
-            } else {
-                await interaction.reply({ content: message, ephemeral: true });
+            if (interaction.isRepliable()) {
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: message, ephemeral: true });
+                } else {
+                    await interaction.reply({ content: message, ephemeral: true });
+                }
             }
         }
     },
