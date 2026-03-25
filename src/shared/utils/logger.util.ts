47789a22bdd59309d1a6b929d8b1049671591267
@felipe.micheletti_hadrons <felipe.micheletti_hadrons@prestador.globo>
2026-03-25 16:42:17 -0300

    feat: updating logger util to show logs more clear

--- src/shared/utils/logger.util.ts
+++ src/shared/utils/logger.util.ts
@@ -1,14 +1,29 @@
+import chalk from "chalk";
+
 const logger = {
+    timeStamp() {
+        const date = new Date();
+        const pad = (num: number) => num.toString().padStart(2, '0');
+
+        const hours = pad(date.getUTCHours() - 3); // Ajuste para UTC-3
+        const minutes = pad(date.getUTCMinutes());
+        const seconds = pad(date.getUTCSeconds());
+
+        const timeOnly = `${hours}:${minutes}:${seconds}`;
+
+        return timeOnly;
+    },
+
     info(message: string, ...args: unknown[]) {
-        console.log(`[INFO] ${message}`, ...args);
+        console.log(chalk.blue(`[INFO] ${this.timeStamp()} ${message}`, ...args));
     },
 
     warn(message: string, ...args: unknown[]) {
-        console.warn(`[WARN] ${message}`, ...args);
+        console.warn(chalk.yellow(`[WARN] ${this.timeStamp()} ${message}`, ...args));
     },
 
     error(message: string, ...args: unknown[]) {
-        console.error(`[ERROR] ${message}`, ...args);
+        console.error(chalk.red.bold(`[ERROR] ${message}`, ...args));
     },
 };
 
